90770dd4065f9d097bbb7ec847e85e3edb109191
Huba <hieu.dcu@gmail.com>
2024-09-16 14:13:02 +1200

    Fix: View Compilation via NuGet

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
 
 var app = builder.Build();
 
--- obj/Debug/net8.0/FitnessWebApp-ASP.NETCore.AssemblyInfo.cs
+++ obj/Debug/net8.0/FitnessWebApp-ASP.NETCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessWebApp-ASP.NETCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cda1d9ebe960c2f3fc83304ac6269b223038f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessWebApp-ASP.NETCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessWebApp-ASP.NETCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
